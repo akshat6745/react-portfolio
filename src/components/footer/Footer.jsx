24edd4d4c394595ac1acc855c924b98478cccec6
Akshat Aggarwal <77236290+akshat6745@users.noreply.github.com>
2022-07-02 09:02:19 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer id="footer">
       <a href="#" className="footer__logo">
-        My
+        My Portfolio
       </a>
       <ul className="permalinks">
         <l1>
